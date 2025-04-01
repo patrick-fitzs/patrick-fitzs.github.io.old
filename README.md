4fd577f6d143494be764f81aa43aa835ded5196f
Patrick fitzsimons <patrickfitzs17@hotmail.com>
2025-04-01 17:46:23 +0100

    Basic setup for my index/home page Cleaned up CSS for basic colouring, and image placement with links to socials and repo's Added js script which i use to blend two colours for a smooth colour transition from top to bottom of page

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # patrick-fitzs.github.io
+
+Starting a personal website for personal interest / projects
+
+Added so far:
+-
+- ✅ README (wow)
+
+
+- (🚧Under construction🚧) index consisting of a home page, will contain links to Github and LinkedIn. A simple scroll animation to make scrolling fluid.
+
+
+- (🚧Under construction🚧) My Projects section. Will contain some projects i have completed over time with info.
---
+++ script.js
@@ -0,0 +1,24 @@
+// function to blend the two RGB colours
+function interpolateColor(color1, color2, factor) {
+    // below extracts the rgb values from strings to int
+    const c1 = color1.match(/\d+/g).map(Number);
+    const c2 = color2.match(/\d+/g).map(Number);
+    // this transitions between the two colours (factor=0=colour1) and (factor=1=colour2)
+    const result = c1.map((c, i) => Math.round(c + (c2[i] - c) * factor));
+    // return as string
+    return `rgb(${result[0]}, ${result[1]}, ${result[2]})`;
+}
+// listener to run when user scrolls
+document.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY; // gets bigger as user scrolls down
+    const docHeight = document.body.scrollHeight - window.innerHeight; // total scrollable height
+    const scrollFraction = Math.min(scrollTop / docHeight, 1); //check how far has been scrolled (0 - 1)
+
+    // start and end colours
+    const startColor = "rgb(240, 240, 240)"; // light grey
+    const endColor = "rgb(128, 128, 128)";   // soft blue
+
+    // calculates the blended colour based on the scrollFraction
+    const newColor = interpolateColor(startColor, endColor, scrollFraction);
+    document.body.style.backgroundColor = newColor; // applies to body
+});
